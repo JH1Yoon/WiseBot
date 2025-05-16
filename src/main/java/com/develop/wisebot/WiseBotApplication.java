01dd07aafc3fd316d98a113f2683c914f9538d1c
JH1Yoon <wlgus4589@naver.com>
2025-05-16 18:19:02 +0900

    init commit

---
+++ src/main/java/com/develop/wisebot/WiseBotApplication.java
@@ -0,0 +1,13 @@
+package com.develop.wisebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseBotApplication.class, args);
+    }
+
+}
